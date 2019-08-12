d4dc960122efb64bca4aeaa30bf7de05fdb2a5bf
vitvvn <wnpq@rambler.ru>
2019-08-12 22:12:12 +0600

    Added working list tags endpoint.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@example.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 # noinspection PyUnresolvedReferences,PyUnresolvedReferences
 class ModelTests(TestCase):
+
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
         email = 'testn@example.com'
@@ -37,3 +45,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan',
+        )
+
+        self.assertEqual(str(tag), tag.name)
